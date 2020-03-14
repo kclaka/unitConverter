b466a9a4a450f1ec709ba093934906c9a4868cbe
kclaka <kennyigbechi@gmail.com>
2020-03-14 10:42:29 -0700

    Add project files.

---
+++ unitConverter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace unitConverter
+{
+
+
+    public class unitConverter
+    {
+        int ratio;
+
+        public unitConverter(int unitRatio)
+        {
+            ratio = unitRatio;
+
+        }
+
+        public int Convert(int unit)
+        {
+            return unit * ratio;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unitConverter feet2inchesConverter = new unitConverter(12);
+            unitConverter milestofeetConver = new unitConverter(5280);
+
+            Console.WriteLine(feet2inchesConverter.Convert(200));
+            Console.WriteLine(milestofeetConver.Convert(20));
+
+
+        }
+    }
+}
